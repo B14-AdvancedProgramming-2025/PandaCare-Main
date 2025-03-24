127e3eb577173355c2175531c3828e18529c6d83
DawnFall19 <ignasius.michael09@gmail.com>
2025-03-24 14:48:58 +0700

    Initialize Project

---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/PandaCareApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b14.pandacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/b14/pandacare/PandaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b14.pandacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
