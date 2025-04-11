9514c4ca9d225803d59d63fd84e6850bc96a97e0
DawnFall19 <ignasius.michael09@gmail.com>
2025-04-11 20:29:49 +0700

    [RED] Add test and skeleton for design pattern

---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/model/Wallet.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.model;
+
+public class Wallet {
+
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/strategy/TransactionStrategy.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.strategy;
+
+public interface TransactionStrategy {
+
+}
