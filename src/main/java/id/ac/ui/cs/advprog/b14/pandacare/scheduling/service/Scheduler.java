ae11fe0681df7d9081738f042c69e06dfd4cd59a
Ragnall16 <ragnall.malfath@gmail.com>
2025-04-11 19:27:40 +0700

    [GREEN] Implement Strategy Pattern on Scheduling

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/service/Scheduler.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/service/Scheduler.java
@@ -1,4 +1,17 @@
 package id.ac.ui.cs.advprog.b14.pandacare.scheduling.service;
 
+import id.ac.ui.cs.advprog.b14.pandacare.scheduling.model.Appointment;
+import id.ac.ui.cs.advprog.b14.pandacare.scheduling.model.AppointmentRequest;
+import id.ac.ui.cs.advprog.b14.pandacare.scheduling.strategy.SchedulingStrategy;
+
 public class Scheduler {
+    private final SchedulingStrategy strategy;
+
+    public Scheduler(SchedulingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public Appointment schedule(AppointmentRequest request) {
+        return strategy.schedule(request);
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/strategy/SchedulingStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/strategy/SchedulingStrategy.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.b14.pandacare.scheduling.strategy;
 
+import id.ac.ui.cs.advprog.b14.pandacare.scheduling.model.Appointment;
+import id.ac.ui.cs.advprog.b14.pandacare.scheduling.model.AppointmentRequest;
+
 public interface SchedulingStrategy {
-    
+    Appointment schedule(AppointmentRequest request);
 }
