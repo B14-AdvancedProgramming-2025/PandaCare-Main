ab4f827b80a4e5b30b900babcde1cf31a5cdf8fa
Ragnall16 <ragnall.malfath@gmail.com>
2025-04-11 19:25:01 +0700

    [RED] Add Scheduling Strategy Pattern Skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/model/Appointment.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.b14.pandacare.scheduling.model;
+
+public class Appointment {
+    
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/service/Scheduler.java
@@ -0,0 +1,4 @@
+package id.ac.ui.cs.advprog.b14.pandacare.scheduling.service;
+
+public class Scheduler {
+}
