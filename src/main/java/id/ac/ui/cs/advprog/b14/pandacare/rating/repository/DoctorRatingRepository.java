d9ca5a946931be43b63676bc35a09808a5b5ca02
asShidqi <fayyed76@gmail.com>
2025-05-02 20:03:22 +0700

    [RED] skeleton for DoctorRatingRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/rating/repository/DoctorRatingRepository.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.b14.pandacare.rating.repository;
+
+import id.ac.ui.cs.advprog.b14.pandacare.rating.model.DoctorRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRatingRepository extends JpaRepository<DoctorRating, Long> {
+}
