4cca6b5e7a87a4e9e4a474492358de2b2b8a3c13
asShidqi <fayyed76@gmail.com>
2025-05-02 20:18:44 +0700

    [RED] skeleton for DoctorRatingService

---
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/rating/service/DoctorRatingService.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.b14.pandacare.rating.service;
+
+import id.ac.ui.cs.advprog.b14.pandacare.rating.model.DoctorRating;
+import id.ac.ui.cs.advprog.b14.pandacare.rating.repository.DoctorRatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorRatingService {
+
+    private final DoctorRatingRepository doctorRatingRepository;
+
+    @Autowired
+    public DoctorRatingService(DoctorRatingRepository doctorRatingRepository) {
+        this.doctorRatingRepository = doctorRatingRepository;
+    }
+
+
+    public void saveRating(DoctorRating doctorRating) {
+    }
+}
