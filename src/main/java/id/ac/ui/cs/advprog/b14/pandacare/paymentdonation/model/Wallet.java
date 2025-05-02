5ea7c45bb75cb07f782f54d003bd7e510016a41a
DawnFall19 <ignasius.michael09@gmail.com>
2025-05-02 20:29:07 +0700

    [GREEN] Adding wallet model

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/model/Wallet.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/model/Wallet.java
@@ -1,5 +1,48 @@
 package id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.model;
 
-public class Wallet {
+import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double balance = 0.0;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL)
+    private List<Transaction> transactions = new ArrayList<>();
+
+    protected Wallet() {}
+
+    public Wallet(User user) {
+        this.user = user;
+    }
+
+    public boolean deductBalance(Double amount) {
+        if (amount <= 0 || amount > balance) {
+            return false;
+        }
+        this.balance -= amount;
+        return true;
+    }
+
+    public boolean addBalance(Double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        this.balance += amount;
+        return true;
+    }
 }
