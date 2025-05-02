20db966102a5a38904311f40ee3b30b88a765dd7
Ragnall16 <ragnall.malfath@gmail.com>
2025-05-02 19:43:41 +0700

    [GREEN] Implement Consultation model

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/model/Consultation.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/model/Consultation.java
@@ -1,6 +1,28 @@
 package id.ac.ui.cs.advprog.b14.pandacare.scheduling.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "consultations")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Consultation {
-
+    
+    @Id
+    private String id;
+    
+    @Column(name = "caregiver_id", nullable = false)
+    private String caregiverId;
+    
+    @Column(name = "pacilian_id", nullable = false)
+    private String pacilianId;
+    
+    @Column(name = "schedule_time", nullable = false)
+    private String scheduleTime;
+    
+    @Column(nullable = false)
+    private String status;
 }
