50d577fbabce74248025ae88075a37f6267b458e
Ragnall16 <ragnall.malfath@gmail.com>
2025-05-02 19:10:59 +0700

    Restart

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/scheduling/strategy/SchedulingStrategy.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.b14.pandacare.scheduling.strategy;
-
-import java.util.List;
-
-public interface SchedulingStrategy {
-    boolean isScheduleAvailable(String proposedSchedule, List<String> existingSchedules);
-}
