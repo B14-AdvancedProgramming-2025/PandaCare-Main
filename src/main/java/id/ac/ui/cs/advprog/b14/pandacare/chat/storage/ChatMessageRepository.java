ddc80e44e74f9ef3b66521796eb47824333bc0da
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-05-09 19:48:50 +0700

    [GREEN] Convert ChatMessageRepository to interface extending JpaRepository and update message retrieval method.

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/storage/ChatMessageRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/storage/ChatMessageRepository.java
@@ -1,23 +1,13 @@
 package id.ac.ui.cs.advprog.b14.pandacare.chat.storage;
 
 import id.ac.ui.cs.advprog.b14.pandacare.chat.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Repository
-public class ChatMessageRepository {
-    private final Map<String, List<ChatMessage>> messagesByRoomId = new HashMap<>();
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, String> {
     
-    public void save(String roomId, ChatMessage message) {
-        List<ChatMessage> messages = messagesByRoomId.computeIfAbsent(roomId, k -> new ArrayList<>());
-        messages.add(message);
-    }
-    
-    public List<ChatMessage> findByRoomId(String roomId) {
-        return messagesByRoomId.getOrDefault(roomId, new ArrayList<>());
-    }
+    List<ChatMessage> findByChatRoomRoomId(String roomId);
 } 
