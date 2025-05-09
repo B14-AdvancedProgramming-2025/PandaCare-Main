867dd5ef45b9dfc4b250970038dbe59dbf2816e7
asShidqi <fayyed76@gmail.com>
2025-05-09 19:19:33 +0700

    [RED] Make rating model use UUID for primary key

--- src/test/java/id/ac/ui/cs/advprog/b14/pandacare/rating/model/DoctorRatingTest.java
+++ src/test/java/id/ac/ui/cs/advprog/b14/pandacare/rating/model/DoctorRatingTest.java
@@ -1,72 +1,63 @@
 package id.ac.ui.cs.advprog.b14.pandacare.rating.model;
 
-import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.Caregiver;
-import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.Pacilian;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DoctorRatingTest {
 
     private DoctorRating doctorRating;
-    private Pacilian pacilian;
-    private Caregiver caregiver;
 
     @BeforeEach
     public void setUp() {
-        pacilian = new Pacilian(
-                "paci@gmail.com", "p@ss", "Paci Test", "1234567890",
-                "Jl. Paci", "08123456789", List.of("Alergi")
-        );
-
-        caregiver = new Caregiver(
-                "care@gmail.com", "c@re", "Caregiver Test", "0987654321",
-                "Jl. Care", "08987654321", "Umum", List.of("Senin", "Selasa")
-        );
-
         doctorRating = new DoctorRating();
     }
 
     @Test
-    public void testSetAndGetPacilian() {
-        doctorRating.setPacilian(pacilian);
-        assertEquals(pacilian, doctorRating.getPacilian());
+    public void testUUIDIsGeneratedInConstructor() {
+        DoctorRating rating = new DoctorRating("caregiver-001", "pacilian-001", 4, "Great doctor!");
+        assertNotNull(rating.getId());
+        assertFalse(rating.getId().isEmpty());
     }
 
     @Test
-    public void testSetAndGetCaregiver() {
-        doctorRating.setCaregiver(caregiver);
-        assertEquals(caregiver, doctorRating.getCaregiver());
+    public void testSetAndGetCaregiverId() {
+        doctorRating.setCaregiverId("caregiver-123");
+        assertEquals("caregiver-123", doctorRating.getCaregiverId());
+    }
+
+    @Test
+    public void testSetAndGetPacilianId() {
+        doctorRating.setPacilianId("pacilian-456");
+        assertEquals("pacilian-456", doctorRating.getPacilianId());
     }
 
     @Test
     public void testSetAndGetValue() {
-        doctorRating.setValue(4);
-        assertEquals(4, doctorRating.getValue());
+        doctorRating.setValue(5);
+        assertEquals(5, doctorRating.getValue());
     }
 
     @Test
     public void testSetAndGetComment() {
-        String comment = "Dokternya sangat ramah dan membantu!";
+        String comment = "Very helpful and kind!";
         doctorRating.setComment(comment);
         assertEquals(comment, doctorRating.getComment());
     }
 
     @Test
     public void testFullObject() {
-        doctorRating.setPacilian(pacilian);
-        doctorRating.setCaregiver(caregiver);
-        doctorRating.setValue(5);
-        doctorRating.setComment("Excellent service!");
+        doctorRating.setCaregiverId("caregiver-xyz");
+        doctorRating.setPacilianId("pacilian-xyz");
+        doctorRating.setValue(3);
+        doctorRating.setComment("Average service.");
 
         assertAll("DoctorRating Full Test",
-                () -> assertEquals(pacilian, doctorRating.getPacilian()),
-                () -> assertEquals(caregiver, doctorRating.getCaregiver()),
-                () -> assertEquals(5, doctorRating.getValue()),
-                () -> assertEquals("Excellent service!", doctorRating.getComment())
+                () -> assertEquals("caregiver-xyz", doctorRating.getCaregiverId()),
+                () -> assertEquals("pacilian-xyz", doctorRating.getPacilianId()),
+                () -> assertEquals(3, doctorRating.getValue()),
+                () -> assertEquals("Average service.", doctorRating.getComment())
         );
     }
 }
