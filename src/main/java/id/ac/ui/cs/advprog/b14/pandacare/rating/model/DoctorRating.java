97024a3be3b4f75bf9a7b7ab2edb1d135e728b7c
asShidqi <fayyed76@gmail.com>
2025-05-09 19:21:02 +0700

    [GREEN] Make implementation mode use UUID

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/rating/model/DoctorRating.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/rating/model/DoctorRating.java
@@ -1,24 +1,26 @@
 package id.ac.ui.cs.advprog.b14.pandacare.rating.model;
 
-import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.Caregiver;
-import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.Pacilian;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class DoctorRating {
-    private Caregiver caregiver; // yang diberi rating
-    private Pacilian pacilian;   // yang memberi rating
+    private String id;
+    private String caregiverId;
+    private String pacilianId;
     private int value;
     private String comment;
 
-    // Getters & Setters
-    public Caregiver getCaregiver() { return caregiver; }
-    public void setCaregiver(Caregiver caregiver) { this.caregiver = caregiver; }
-
-    public Pacilian getPacilian() { return pacilian; }
-    public void setPacilian(Pacilian pacilian) { this.pacilian = pacilian; }
-
-    public int getValue() { return value; }
-    public void setValue(int value) { this.value = value; }
-
-    public String getComment() { return comment; }
-    public void setComment(String comment) { this.comment = comment; }
+    public DoctorRating(String caregiverId, String pacilianId, int value, String comment) {
+        this.id = UUID.randomUUID().toString();  // generate ID otomatis
+        this.caregiverId = caregiverId;
+        this.pacilianId = pacilianId;
+        this.value = value;
+        this.comment = comment;
+    }
 }
