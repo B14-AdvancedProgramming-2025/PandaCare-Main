f4e49223c4cc272a76e5d2bdb416feb848a5107f
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-05-09 19:45:18 +0700

    [REFACTOR] Add JPA entity with fields, Lombok annotations, constructors

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/model/ChatMessage.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/model/ChatMessage.java
@@ -1,24 +1,47 @@
 package id.ac.ui.cs.advprog.b14.pandacare.chat.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "chat_messages")
 @Getter
 @Setter
 @NoArgsConstructor
 public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
     private String sender;
     private String recipient;
     private String content;
+    
+    @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime timestamp;
+    
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private ChatRoom chatRoom;
     
     public ChatMessage(String sender, String recipient, String content, LocalDateTime timestamp) {
         this.sender = sender;
         this.recipient = recipient;
         this.content = content;
         this.timestamp = timestamp;
+    }
+    
+    public ChatMessage(String sender, String recipient, String content, LocalDateTime timestamp, ChatRoom chatRoom) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+        this.timestamp = timestamp;
+        this.chatRoom = chatRoom;
     }
 } 
