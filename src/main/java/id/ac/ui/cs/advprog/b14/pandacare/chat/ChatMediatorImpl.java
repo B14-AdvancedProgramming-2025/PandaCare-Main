306c4c4dd428d08f5a578e180dc222acb0a968dc
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-05-16 18:17:02 +0700

    [REFACTOR] Deleted and commented redundant codes

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/ChatMediatorImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/ChatMediatorImpl.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatMediatorImpl implements ChatMediator {
-    private List<ChatUser> users;
+    // private List<ChatUser> users;
     
     public ChatMediatorImpl() {
-        this.users = new ArrayList<>();
+        // this.users = new ArrayList<>();
     }
     
-    @Override
-    public void sendMessage(String message, ChatUser sender) {
-        for (ChatUser user : users) {
-            // Don't send message back to the sender
-            if (user != sender) {
-                user.receive(message);
-            }
-        }
-    }
+    // @Override
+    // public void sendMessage(String message, ChatUser sender) {
+    //     for (ChatUser user : users) {
+    //         // Don't send message back to the sender
+    //         if (user != sender) {
+    //             user.receive(message);
+    //         }
+    //     }
+    // }
     
-    @Override
-    public void addUser(ChatUser user) {
-        users.add(user);
-    }
+    // @Override
+    // public void addUser(ChatUser user) {
+    //     users.add(user);
+    // }
 } 
