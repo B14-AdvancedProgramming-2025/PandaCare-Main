c429971730e1525bab2090ef26198ca5cf1e60d1
DawnFall19 <ignasius.michael09@gmail.com>
2025-05-25 06:57:52 +0700

    [GREEN] Revising wallet repository logic

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/repository/WalletRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/repository/WalletRepository.java
@@ -1,13 +1,11 @@
 package id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.repository;
 
-import id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.model.Wallet;
 import id.ac.ui.cs.advprog.b14.pandacare.authentication.model.User;
+import id.ac.ui.cs.advprog.b14.pandacare.paymentdonation.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-    Optional<Wallet> findByUser(User user);
+    Wallet findByUser(User user);
 }
