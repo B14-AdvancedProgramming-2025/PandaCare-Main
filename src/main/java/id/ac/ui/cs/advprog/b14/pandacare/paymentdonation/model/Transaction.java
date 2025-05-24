30447387b73164ecab9f91d21394a70cd67af669
DawnFall19 <ignasius.michael09@gmail.com>
2025-05-25 06:32:59 +0700

    [GREEN] Modifying logic for transaction

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/model/Transaction.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/paymentdonation/model/Transaction.java
@@ -7,22 +7,37 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name= "transactions")
+@Inheritance(strategy = InheritanceType.JOINED)
 @Getter @Setter
-public class Transaction {
+public abstract class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Double amount;
+
+    @Column(nullable = false)
     private String description;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionType type;
 
+    @Column(nullable = false)
     private LocalDateTime timestamp = LocalDateTime.now();
-    private String provider;
 
     @ManyToOne
     @JoinColumn(name = "wallet_id")
     private Wallet wallet;
+
+    protected Transaction() {}
+
+    public Transaction(Double amount, String description, Wallet wallet, TransactionType type) {
+        this.amount = amount;
+        this.description = description;
+        this.wallet = wallet;
+        this.type = type;
+    }
 }
