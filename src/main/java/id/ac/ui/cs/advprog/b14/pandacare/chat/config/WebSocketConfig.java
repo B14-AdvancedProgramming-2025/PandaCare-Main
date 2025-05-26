7581cf953293819d00c21ae3faf1b766dd3a87a3
absolutepraya <daffaabhiprayaputra@gmail.com>
2025-05-26 22:15:04 +0700

    [REFACTOR] Remove unused imports from AuthenticationFacade and WebSocketConfig

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/config/WebSocketConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/config/WebSocketConfig.java
@@ -1,8 +1,6 @@
 package id.ac.ui.cs.advprog.b14.pandacare.chat.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
