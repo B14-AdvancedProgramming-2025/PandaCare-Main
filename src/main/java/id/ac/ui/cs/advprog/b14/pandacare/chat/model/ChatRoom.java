01f4489cb662a7321d2e5b3ef34340df17b2cb7a
absolutepraya <daffaabhiprayaputra@gmail.com>
2025-05-27 12:56:06 +0700

    [REFACTOR] Add @JsonIgnore annotation to messages list in ChatRoom to prevent serialization issues

--- src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/model/ChatRoom.java
+++ src/main/java/id/ac/ui/cs/advprog/b14/pandacare/chat/model/ChatRoom.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,7 @@ public class ChatRoom {
     private String caregiverId;
     
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<ChatMessage> messages;
     
     public ChatRoom(String roomId, String pacilianId, String caregiverId) {
